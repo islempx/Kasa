9199959f7d7680f316725dd47bb8cc2ceb14aa3a
RemadiIslem <islem.remadi69@gmail.com>
2026-03-22 14:18:06 +0100

    add Error && responsive

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -2,7 +2,13 @@ import { Link } from 'react-router-dom'
 import './Error.css'
 
 function Error() {
-  return <div>404</div>
+  return <div>
+  
+  
+  <h1 className='number'>404</h1>
+   <h2 className='Oups'> Oups! La page que vous demandez n'existe pas.</h2>
+   <Link to="/" className="link-home">Retourner sur la page d’accueil</Link>
+          </div>
 }
 
 export default Error
